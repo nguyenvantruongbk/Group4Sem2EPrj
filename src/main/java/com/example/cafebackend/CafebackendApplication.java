e86ae1d5b24df6d6be7559c7f8af7760be4b12e3
Nguyen_Van_Truong <truongnv87@gmail.com>
2024-11-12 20:00:18 +0700

    backEndInit

---
+++ src/main/java/com/example/cafebackend/CafebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafebackendApplication.class, args);
+	}
+
+}
